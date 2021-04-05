bf6749cc288726a30ce02c6cae497d206d6b1a7a
Imaad Syed <30536128+isyedjr@users.noreply.github.com>
2021-04-05 02:05:40 -0400

    Update viewer.py

--- Software/viewer.py
+++ Software/viewer.py
@@ -1,5 +1,8 @@
 # Viewer.py
 # Imaad Syed
 # 4/05/2021
+#
+# This program is used to manage the PiCamera, and apply the neccesary imaging filters. This program also takes pictures and records videos.
+#
 
 from time import sleep
