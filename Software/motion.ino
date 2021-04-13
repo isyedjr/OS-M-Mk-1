27eeea10cdd7c1266c64ad220efa9554ef47405a
Imaad Syed <isyedjr@gmail.com>
2021-04-13 20:10:07 +0000

    Update motion.ino

--- Software/motion.ino
+++ Software/motion.ino
@@ -1,8 +1,159 @@
 /* motion.ino
- * 
+ * Lafe Spietz, Imaad Syed
+ * 2/17/2021
  * This is the program that controls the motion stage. In order to run this, compile and upload to the Arduino, and then use the joystick and buttons.
  * Need to download from Pi
  *
 */
 
-#define joyX A0
+int delayus = 150;//delayMicroseconds(delayus);
+
+//connect these pins to the MP6500 stepper motor control board:
+int dirPin3 = 8;
+int stepPin3 = 9;
+int enPin3 = 10;
+int dirPin1 = 5;
+int stepPin1 = 6;
+int enPin1 = 7;
+int dirPin2 = 3;
+int stepPin2 = 4;
+int enPin2 = A0;
+
+void setup() { 
+    pinMode(11,INPUT); 
+    pinMode(12,INPUT);
+    pinMode(A4, INPUT);
+    pinMode(A5, INPUT);
+
+    pinMode(dirPin1,OUTPUT);
+    pinMode(stepPin1,OUTPUT);
+    pinMode(enPin1,OUTPUT);
+    pinMode(dirPin2,OUTPUT);
+    pinMode(stepPin2,OUTPUT);
+    pinMode(enPin2,OUTPUT);
+    pinMode(dirPin3,OUTPUT);
+    pinMode(stepPin3,OUTPUT);
+    pinMode(enPin3,OUTPUT);
+
+    digitalWrite(dirPin1,LOW);
+    digitalWrite(stepPin1,LOW);
+    digitalWrite(enPin1,HIGH);
+    digitalWrite(dirPin2,LOW);
+    digitalWrite(stepPin2,LOW);
+    digitalWrite(enPin2,HIGH);
+    digitalWrite(dirPin3,LOW);
+    digitalWrite(stepPin3,LOW);
+    digitalWrite(enPin3,HIGH);
+    
+    Serial.begin(9600);
+}
+
+void loop() {
+
+  if((abs(analogRead(A5) - 511) - 500) > 0){
+      if((analogRead(A5) - 511) > 0){
+        moveDown(20);
+        delay(10);
+       } else if ((analogRead(A5) - 511) < 0) {
+        moveUp(20);
+        delay(10);
+      }  
+  }
+  if((abs(analogRead(A4) - 511) - 500) > 0){
+      if((analogRead(A4) - 511) > 0){
+        moveRight(20);
+        delay(10);
+      } else if((analogRead(A4) - 511) < 0){
+        moveLeft(25);
+        delay(5);
+      }  
+  }
+  
+  if(digitalRead(11) == HIGH){
+    moveAway(7);
+    delay(7);
+  } else if(digitalRead(12) == HIGH){
+    moveTowards(10);
+    delay(5);
+  }
+
+ 
+}
+
+void moveLeft(int nSteps){
+     digitalWrite(dirPin1,LOW);
+     digitalWrite(enPin1,LOW);
+     
+     for(int index = 0;index < nSteps;index++){
+       digitalWrite(stepPin1,HIGH);
+       delayMicroseconds(delayus); 
+       digitalWrite(stepPin1,LOW);
+       delayMicroseconds(delayus); 
+     }          
+     digitalWrite(enPin1,HIGH);   
+}
+
+void moveRight(int nSteps){
+     digitalWrite(dirPin1,HIGH);
+     digitalWrite(enPin1,LOW);
+     
+     for(int index = 0;index < nSteps;index++){
+       digitalWrite(stepPin1,HIGH);
+       delayMicroseconds(delayus); 
+       digitalWrite(stepPin1,LOW);
+       delayMicroseconds(delayus); 
+     }          
+     digitalWrite(enPin1,HIGH);   
+}
+
+void moveDown(int nSteps){
+      digitalWrite(dirPin3,LOW);
+     digitalWrite(enPin3,LOW);
+     
+     for(int index = 0;index < nSteps;index++){
+       digitalWrite(stepPin3,HIGH);
+       delayMicroseconds(delayus); 
+       digitalWrite(stepPin3,LOW);
+       delayMicroseconds(delayus); 
+     }          
+     digitalWrite(enPin1,HIGH);   
+}
+
+void moveUp(int nSteps){
+     digitalWrite(dirPin3,HIGH);
+     digitalWrite(enPin3,LOW);
+     
+     for(int index = 0;index < nSteps;index++){
+       digitalWrite(stepPin3,HIGH);
+       delayMicroseconds(delayus); 
+       digitalWrite(stepPin3,LOW);
+       delayMicroseconds(delayus); 
+     }          
+     digitalWrite(enPin3,HIGH);    
+}
+
+void moveAway(int nSteps){
+     digitalWrite(dirPin2,LOW);
+     digitalWrite(enPin2,LOW);
+     
+     for(int index = 0; index < nSteps; index++){
+       digitalWrite(stepPin2,HIGH);
+       delayMicroseconds(delayus); 
+       digitalWrite(stepPin2,LOW);
+       delayMicroseconds(delayus); 
+     }          
+     digitalWrite(enPin2,HIGH);   
+}
+
+void moveTowards(int nSteps){
+     digitalWrite(dirPin2,HIGH);
+     digitalWrite(enPin2,LOW);
+     
+     for(int index = 0; index < nSteps; index++){
+       digitalWrite(stepPin2,HIGH);
+       delayMicroseconds(delayus); 
+       digitalWrite(stepPin2,LOW);
+       delayMicroseconds(delayus); 
+     }          
+     digitalWrite(enPin2,HIGH);   
+}
